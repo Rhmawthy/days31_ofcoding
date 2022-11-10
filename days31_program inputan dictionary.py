8e1667c46372d133ec4ca63f5fc19129311d81b4
Rahmawati <115470125+Rhmawthy@users.noreply.github.com>
2022-11-10 05:14:10 -0800

    Add files via upload

---
+++ days31_program inputan dictionary.py
@@ -0,0 +1,15 @@
+while True:
+     nama = input ("masukkan nama anda")
+     nim = int(input ("masukkan Nim anda"))
+
+     dt_mhs = {
+        nama : nim    
+     }
+     
+     print (dt_mhs)
+
+
+
+    
+
+
